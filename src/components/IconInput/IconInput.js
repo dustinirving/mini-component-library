0c7d941a1debfebc690b74167871a6813fb3fc46
Dustin Irving <dustin.irving@gmail.com>
2021-06-14 20:55:25 -0400

    update component styles

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,71 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const SIZES = {
+  small: 16,
+  large: 24,
+};
+
+const Wrapper = styled.label`
+  position: relative;
+  display: block;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  border: none;
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+  
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+
+const SmallInput = styled(TextInput)`
+  height: 24px;
+  font-size: ${14 / 16}rem;
+  border-bottom: 1px solid ${COLORS.black};
+  padding-left: 24px;
+`;
+
+const LargeInput = styled(TextInput)`
+  height: 36px;
+  font-size: ${18 / 16}rem;
+  border-bottom: 2px solid ${COLORS.black};
+  padding-left: 36px;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 2px;
+  left: 0;
+  pointer-events: none;
+  color: inherit;
+`;
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const sizeNumber = SIZES[size];
+  const Input = size === 'small'
+    ? SmallInput
+    : LargeInput;
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Input placeholder={placeholder} />
+      <IconWrapper>
+        <Icon id={icon} size={sizeNumber} width={width} />
+      </IconWrapper>
+    </Wrapper>
+  );
 };
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,52 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--height': '8px',
+    '--border-radius': '4px',
+  },
+  medium: {
+    '--height': '12px',
+    '--border-radius': '4px',
+  },
+  large: {
+    '--height': '24px',
+    '--border-radius': '8px',
+    '--padding': '4px'
+  },
+};
+
+const Wrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  height: var(--height);
+  border-radius: var(--border-radius);
+  width: 270px;
+  padding: var(--padding);
+  overflow: hidden;
+`;
+
+const Progress = styled.div`
+  border-radius: 4px 0 0 4px;
+  background: ${COLORS.primary};
+  width: ${(props) => props.value + '%'};
+  height: 100%;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+  return (
+    <Wrapper
+      role="progressbar"
+      style={styles}
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <VisuallyHidden>{value}</VisuallyHidden>
+      <Progress value={value}></Progress>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
